7c71076d26ac05b612792943c5ff74405708b236
kodEnigma <olamilekanadeleke123@gmail.com>
2026-04-15 18:39:26 +0100

    fix

--- dist/dev/static/chunks/app_layout_tsx_1cf6b850._.js
+++
@@ -1,9 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/[root-of-the-server]__077fd7b5._.css",
-  "static/chunks/node_modules_0ca8d63c._.js",
-  "static/chunks/_ef35c02a._.js"
-],
-    source: "dynamic"
-});
